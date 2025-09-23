a0f2c4c88c62fcd4c673f49bc31728b10265da32
ZARS <ennard209@gmail.com>
2025-09-23 23:30:10 +0300

    Create GWwind1.xmp.cs

---
+++ Content.Client/_RMC14/GWwind1.xmp.cs
@@ -0,0 +1,7 @@
+<Window xmlns="https://robust-toolbox.net/ui"
+        Title="Телепорт призрака"
+        Size="500,600"
+        CanClose="true"
+        CanResize="true">
+    <VBoxContainer Name="GroupContainer" />
+</Window>
