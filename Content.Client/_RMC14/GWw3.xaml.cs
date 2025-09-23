76427d5770a752b04c4ba25438004f9c667e9353
ZARS <ennard209@gmail.com>
2025-09-23 23:31:59 +0300

    Create GWw3.xaml.cs

---
+++ Content.Client/_RMC14/GWw3.xaml.cs
@@ -0,0 +1,6 @@
+<PanelContainer xmlns="https://robust-toolbox.net/ui"
+                StyleClasses="GhostWarpGroup">
+    <VBoxContainer Name="EntryContainer">
+        <Label Name="GroupLabel" />
+    </VBoxContainer>
+</PanelContainer>
