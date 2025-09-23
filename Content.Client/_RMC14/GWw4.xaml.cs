471115cafd82881b03b79d6e0e59f8d20cbbebc7
ZARS <ennard209@gmail.com>
2025-09-24 00:51:11 +0300

    Update GWw4.xaml.cs

--- Content.Client/_RMC14/GWw4.xaml.cs
+++ Content.Client/_RMC14/GWw4.xaml.cs
@@ -1,4 +1,10 @@
 {
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.IoC;
+
+namespace Content.Client._RMC14;
+ 
     public GhostWarpGroup(string name, Color color)
     {
         GroupLabel.Text = name;
