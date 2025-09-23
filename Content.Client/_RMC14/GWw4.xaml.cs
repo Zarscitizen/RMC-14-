9404b9c30758ff6df0080c0a00c1342daf861271
ZARS <ennard209@gmail.com>
2025-09-24 01:03:03 +0300

    Update GWw4.xaml.cs

--- Content.Client/_RMC14/GWw4.xaml.cs
+++ Content.Client/_RMC14/GWw4.xaml.cs
@@ -2,11 +2,20 @@
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Shared.IoC;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface;
+using Robust.Shared.GameObjects;
 
-namespace Content.Client._RMC14;
- 
+namespace Content.Client.RMC.GhostWarp.UI;
+
+public sealed partial class GhostWarpGroup : PanelContainer
+{
+    private Label GroupLabel => FindChild<Label>("GroupLabel");
+    private VBoxContainer EntryContainer => FindChild<VBoxContainer>("EntryContainer");
+
     public GhostWarpGroup(string name, Color color)
     {
+        InitializeComponent();
         GroupLabel.Text = name;
         Modulate = color;
     }
@@ -16,3 +25,4 @@ namespace Content.Client._RMC14;
         EntryContainer.AddChild(entry);
     }
 }
+namespace Content.Client._RMC14;
