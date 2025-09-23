4acef7df58d53756773606a7937d6a4dbd8f7e83
ZARS <ennard209@gmail.com>
2025-09-24 01:02:21 +0300

    Update GWw6.xaml.cs

--- Content.Client/_RMC14/GWw6.xaml.cs
+++ Content.Client/_RMC14/GWw6.xaml.cs
@@ -2,17 +2,27 @@
 using Robust.Client.UserInterface;
 using Robust.Client.UserInterface.Controls;
 using Robust.Shared.IoC;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface;
+using Robust.Shared.GameObjects;
+using Robust.Client.Player;
 
-namespace Content.Client._RMC14;
-    
+namespace Content.Client.RMC.GhostWarp.UI;
+
+public sealed partial class GhostWarpEntry : Button
+{
     public GhostWarpEntry(EntityUid target)
     {
+        InitializeComponent();
         Text = MetaData(target).EntityName;
+
         OnPressed += _ =>
         {
             var ghost = IoCManager.Resolve<IPlayerManager>().LocalPlayer?.ControlledEntity;
             if (ghost != null)
+            {
                 EntityManager.GetSystem<TeleportationSystem>().TryTeleport(ghost.Value, target);
+            }
         };
     }
 }
