65a0c2f3cb6ab35d2089cc2d5ab37b3bf8c4e8f6
ZARS <ennard209@gmail.com>
2025-09-24 00:13:05 +0300

    Update GWw1.0.xaml.cs

--- Content.Client/_RMC14/GWw1.0.xaml.cs
+++ Content.Client/_RMC14/GWw1.0.xaml.cs
@@ -1,9 +1,9 @@
 {
-Window xmlns="https://robust-toolbox.net/ui"
-        Title="Телепорт призрака"
-        Size="500,600"
-        CanClose="true"
-        CanResize="true"
-    <VBoxContainer Name="GroupContainer" 
+Window xmlns="https://robust-toolbox.net/ui";
+        Title="Телепорт призрака";
+        Size="500,600";
+        CanClose="true";
+        CanResize="true";
+    <VBoxContainer Name="GroupContainer";
 Window
 }
