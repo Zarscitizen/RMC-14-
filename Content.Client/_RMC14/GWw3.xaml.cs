b2a4f1c6dc4ce15cedcccd67c434fb5ff96a7717
ZARS <ennard209@gmail.com>
2025-09-24 00:28:44 +0300

    Update and rename GWw3.xaml.cs to GWw3.xaml

--- Content.Client/_RMC14/GWw3.xaml.cs
+++
@@ -1,8 +0,0 @@
-{;
-    PanelContainer xmlns="https://robust-toolbox.net/ui";
-                StyleClasses="GhostWarpGroup";
-    VBoxContainer Name="EntryContainer";
-        Label Name="GroupLabel";
-    VBoxContainer;
-PanelContainer;
-};
