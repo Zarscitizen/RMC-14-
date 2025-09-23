e379ba357fee834a9d2ef2e2c130cc5124b9c70f
ZARS <ennard209@gmail.com>
2025-09-24 00:48:10 +0300

    Update GWw4.xaml.cs

--- Content.Client/_RMC14/GWw4.xaml.cs
+++ Content.Client/_RMC14/GWw4.xaml.cs
@@ -1,8 +1,4 @@
-public sealed partial class GhostWarpGroup : PanelContainer
 {
-    private Label GroupLabel => GetChild<VBoxContainer>("EntryContainer").GetChild<Label>("GroupLabel");
-    private VBoxContainer EntryContainer => GetChild<VBoxContainer>("EntryContainer");
-
     public GhostWarpGroup(string name, Color color)
     {
         GroupLabel.Text = name;
