b89fc72b275b2f252fe420a3db694f1735280778
ZARS <ennard209@gmail.com>
2025-09-24 00:03:50 +0300

    Update GWw5.xaml.cs

--- Content.Client/_RMC14/GWw5.xaml.cs
+++ Content.Client/_RMC14/GWw5.xaml.cs
@@ -1,5 +1,3 @@
-{
-Button xmlns="https://robust-toolbox.net/ui";
-        Name="TeleportButton";
-        StyleClasses="GhostWarpEntry";
-                                       }
+<Button xmlns="https://robust-toolbox.net/ui"
+        Name="TeleportButton"
+        StyleClasses="GhostWarpEntry" />
