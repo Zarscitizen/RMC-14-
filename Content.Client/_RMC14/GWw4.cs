201962ebcf19031708ad8b965f294a4ec9cc67ea
ZARS <ennard209@gmail.com>
2025-09-24 20:05:25 +0300

    Update GWw4.cs

--- Content.Client/_RMC14/GWw4.cs
+++ Content.Client/_RMC14/GWw4.cs
@@ -1,6 +1,5 @@
 foreach (var category in entityData.Categories)
 {
-    var panel = CreateCategoryPanel(category.Name, category.Count);
     foreach (var item in category.Items)
     {
         panel.AddChild(CreateItemLabel(item));
